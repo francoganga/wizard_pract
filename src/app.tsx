706ae39a73b9bebdd2a6921b3c9a8bea0426a144
franco <fganga@ymail.com>
2022-06-09 13:46:55 -0300

    add react-hook-form and first step.

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useState } from "preact/hooks";
-import { Step1, Step2 } from "./components/Step1";
+import { Step1 } from "./components/Step1";
 import { StepInfo } from "./components/step-info";
 
 export function App() {
@@ -9,7 +9,7 @@ export function App() {
     "Datos Personales",
     "Detalle de Contrato",
     "Materias",
-    "Observaciones",
+    "Observaciones"
   ];
 
   const goNext = () => {
@@ -38,24 +38,18 @@ export function App() {
           <div class="card">
             <div class="card-body">
               <form class="mb-3">
-                {step == 0 && (
-                  <div class="step-1">
-                    <div class="row">
-                      <div class="col-12 col-md-6">
-                        <label for="name">Nombre</label>
-                        <input id="name" class="form-control" type="text" />
-                      </div>
-                      <div class="col-12 col-md-6">
-                        <label for="apellido">Apellido</label>
-                        <input class="form-control" id="apellido" type="text" />
-                      </div>
-                    </div>
-                  </div>
-                )}
+                <div
+                  class="step-1"
+                  style={{ display: step === 0 ? "block" : "none" }}
+                >
+                  <Step1 />
+                </div>
                 {step == 1 && <div class="step-1 h-100">este es el paso 2</div>}
                 {step == 2 && <div class="step-1 h-100">este es el paso 3</div>}
                 {step == 3 && <div class="step-1 h-100">este es el paso 4</div>}
               </form>
+              {/*
+
               <div class="d-flex">
                 {step >= 1 && (
                   <button onClick={goBack} class="btn btn-primary">
@@ -68,6 +62,7 @@ export function App() {
                   </button>
                 )}
               </div>
+              */}
             </div>
           </div>
         </div>
--- src/components/Step1.tsx
+++ src/components/Step1.tsx
@@ -1,16 +1,64 @@
+import { useForm } from "react-hook-form";
+
 export function Step1() {
+  type FormData = {
+    name: string;
+    apellido: string;
+    email: string;
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => console.log(data);
 
   return (
-    <div class="h-100 p-5 bg-primary text-light">
-      step1
-    </div>
-  )
-}
-
-export function Step2() {
-  return (
-    <div class="h-100 p-5 bg-primary text-light">
-      step2
-    </div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div class="row mb-5">
+        <div class="col-12 col-md-6">
+          <label for="name">Nombre</label>
+          <input
+            {...register("name", { required: true })}
+            class={`form-control ${errors.name?.type === "required" &&
+              "is-invalid"}`}
+            type="text"
+          />
+          <small class="text-danger my-1">
+            {errors.name?.type === "required" && "Nombre es requerido"}
+          </small>
+        </div>
+        <div class="col-12 col-md-6">
+          <label for="apellido">Apellido</label>
+          <input
+            {...register("apellido", { required: true })}
+            class={`form-control ${errors.apellido?.type === "required" &&
+              "is-invalid"}`}
+            type="text"
+          />
+          <small class="text-danger my-1">
+            {errors.apellido?.type === "required" && "Apellido es requerido"}
+          </small>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-12">
+          <label for="email">Correo Electrónico</label>
+          <input
+            {...register("email", { required: true })}
+            class={`form-control ${errors.email?.type === "required" &&
+              "is-invalid"}`}
+            type="email"
+          />
+          <small class="text-danger my-1">
+            {errors.email?.type === "required" && "Email es requerido"}
+          </small>
+        </div>
+      </div>
+      <div class="d-flex">
+        <button class="btn btn-primary ms-auto">Siguiente</button>
+      </div>
+    </form>
+  );
 }
